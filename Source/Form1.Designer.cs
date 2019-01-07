ee186eb2332ecfe6c6f84c17bd6511493e527006
puste1 <46462109+puste1@users.noreply.github.com>
2019-01-07 23:02:44 +0100

    Add files via upload

---
+++ Source/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace CHPlayer
+{
+    partial class CHPlayer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CHPlayer));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openVideoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openAudioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.setupToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vLCPathToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.installToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.indexToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button_Sel_Video = new System.Windows.Forms.Button();
+            this.button_Sel_Audio = new System.Windows.Forms.Button();
+            this.VideoFile = new System.Windows.Forms.TextBox();
+            this.AudioFile = new System.Windows.Forms.TextBox();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.setupToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(655, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openVideoToolStripMenuItem,
+            this.openAudioToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // openVideoToolStripMenuItem
+            // 
+            this.openVideoToolStripMenuItem.Name = "openVideoToolStripMenuItem";
+            this.openVideoToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.openVideoToolStripMenuItem.Text = "Open Video";
+            this.openVideoToolStripMenuItem.Click += new System.EventHandler(this.openVideoToolStripMenuItem_Click);
+            // 
+            // openAudioToolStripMenuItem
+            // 
+            this.openAudioToolStripMenuItem.Name = "openAudioToolStripMenuItem";
+            this.openAudioToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.openAudioToolStripMenuItem.Text = "Open Audio";
+            this.openAudioToolStripMenuItem.Click += new System.EventHandler(this.openAudioToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // setupToolStripMenuItem
+            // 
+            this.setupToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.vLCPathToolStripMenuItem,
+            this.installToolStripMenuItem});
+            this.setupToolStripMenuItem.Name = "setupToolStripMenuItem";
+            this.setupToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.setupToolStripMenuItem.Text = "Setup";
+            this.setupToolStripMenuItem.Click += new System.EventHandler(this.setupToolStripMenuItem_Click);
+            // 
+            // vLCPathToolStripMenuItem
+            // 
+            this.vLCPathToolStripMenuItem.Name = "vLCPathToolStripMenuItem";
+            this.vLCPathToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.vLCPathToolStripMenuItem.Text = "VLC Path";
+            this.vLCPathToolStripMenuItem.Click += new System.EventHandler(this.vLCPathToolStripMenuItem_Click);
+            // 
+            // installToolStripMenuItem
+            // 
+            this.installToolStripMenuItem.Name = "installToolStripMenuItem";
+            this.installToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.installToolStripMenuItem.Text = "Install Pornhub Plugin";
+            this.installToolStripMenuItem.Click += new System.EventHandler(this.installToolStripMenuItem_Click_1);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.indexToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // indexToolStripMenuItem
+            // 
+            this.indexToolStripMenuItem.Name = "indexToolStripMenuItem";
+            this.indexToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.indexToolStripMenuItem.Text = "Index";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // button_Sel_Video
+            // 
+            this.button_Sel_Video.Location = new System.Drawing.Point(521, 56);
+            this.button_Sel_Video.Name = "button_Sel_Video";
+            this.button_Sel_Video.Size = new System.Drawing.Size(78, 23);
+            this.button_Sel_Video.TabIndex = 1;
+            this.button_Sel_Video.Text = "Select Video";
+            this.button_Sel_Video.UseVisualStyleBackColor = true;
+            this.button_Sel_Video.Click += new System.EventHandler(this.button_Sel_VideoFile_Click);
+            // 
+            // button_Sel_Audio
+            // 
+            this.button_Sel_Audio.Location = new System.Drawing.Point(521, 118);
+            this.button_Sel_Audio.Name = "button_Sel_Audio";
+            this.button_Sel_Audio.Size = new System.Drawing.Size(78, 23);
+            this.button_Sel_Audio.TabIndex = 2;
+            this.button_Sel_Audio.Text = "Select Audio";
+            this.button_Sel_Audio.UseVisualStyleBackColor = true;
+            this.button_Sel_Audio.Click += new System.EventHandler(this.button_Sel_Audio_Click);
+            // 
+            // VideoFile
+            // 
+            this.VideoFile.Location = new System.Drawing.Point(22, 59);
+            this.VideoFile.Name = "VideoFile";
+            this.VideoFile.Size = new System.Drawing.Size(481, 20);
+            this.VideoFile.TabIndex = 3;
+            this.VideoFile.Text = "e:\\Privat Download\\CH Video\\Cock Hero\\CH Flux - Episode 4 hard\\CH Flux - Episode " +
+    "04 StL.mp4";
+            // 
+            // AudioFile
+            // 
+            this.AudioFile.Location = new System.Drawing.Point(22, 120);
+            this.AudioFile.Name = "AudioFile";
+            this.AudioFile.Size = new System.Drawing.Size(481, 20);
+            this.AudioFile.TabIndex = 4;
+            this.AudioFile.Text = "e:\\Privat Download\\CH Video\\Cock Hero\\CH Flux - Episode 4 hard\\CH Flux - Episode " +
+    "04 StL.mp3";
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Location = new System.Drawing.Point(486, 159);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(113, 28);
+            this.buttonPlay.TabIndex = 7;
+            this.buttonPlay.Text = "Play Video in VLC";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Select Video File or Paste URL:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Select Audio File:";
+            // 
+            // CHPlayer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 199);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonPlay);
+            this.Controls.Add(this.AudioFile);
+            this.Controls.Add(this.VideoFile);
+            this.Controls.Add(this.button_Sel_Audio);
+            this.Controls.Add(this.button_Sel_Video);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "CHPlayer";
+            this.Text = "CHPlayer";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openVideoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openAudioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem setupToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vLCPathToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem indexToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem installToolStripMenuItem;
+        private System.Windows.Forms.Button button_Sel_Video;
+        private System.Windows.Forms.Button button_Sel_Audio;
+        private System.Windows.Forms.TextBox VideoFile;
+        private System.Windows.Forms.TextBox AudioFile;
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Source/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Net; 
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CHPlayer
+{
+    public partial class CHPlayer : Form
+    {
+        string sVLC = "";
+        string sVideo = "";
+        string sAudio = "";
+
+        public CHPlayer()
+        {
+            InitializeComponent();
+            // Load save Path to VLC Program
+            sVLC = Properties.Settings.Default.pathVLC;
+            sVideo = Properties.Settings.Default.pathVideo;
+            sAudio = Properties.Settings.Default.pathAudio;
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //*****************************************************************************************
+        // Bottom Select Video   
+        //*****************************************************************************************
+        private void button_Sel_VideoFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Cursor Files|*.mp4";
+            openFileDialog1.Title = "Select a MP4 Video File";
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                // Assign the cursor in the Stream to the Form's Cursor property.  
+                this.VideoFile.Text = openFileDialog1.FileName;
+            }
+        }
+
+        //*****************************************************************************************
+        // Bottom Select Audio  
+        //*****************************************************************************************
+        private void button_Sel_Audio_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Cursor Files|*.mp3";
+            openFileDialog1.Title = "Select a MP3 Audio File";
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                // Assign the cursor in the Stream to the Form's Cur
+
+                this.AudioFile.Text = openFileDialog1.FileName;
+            }
+        }
+
+        //*****************************************************************************************
+        // Bottom Play Start VLC 
+        //*****************************************************************************************
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            //string pathVLC = "C:\\Program Files\\VideoLAN\\VLC\\vlc.exe";
+            
+            System.Diagnostics.Process VLC = new System.Diagnostics.Process();
+            //VLC.StartInfo.FileName = pathVLC; 
+            VLC.StartInfo.FileName = sVLC;
+
+                                    
+            // Set Argument for VLC (Select Estim Audio for selected Video)
+            VLC.StartInfo.Arguments = string.Format("\"{0}\" {1}\" {2}\" {3}", 
+                 @VideoFile.Text, 
+                 " --input-slave ", 
+                 AudioFile.Text, 
+                 " --audio-track=1");
+
+            
+            // Save Path to VLC program 
+            Properties.Settings.Default.pathVideo = @VideoFile.Text;
+            Properties.Settings.Default.pathVideo = @AudioFile.Text;
+            // Start VLC
+            VLC.Start();
+        }
+
+
+        //*****************************************************************************************
+        // Exit Aplcation  
+        //*****************************************************************************************
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //*****************************************************************************************
+        // Set link to VLC Program
+        //*****************************************************************************************
+        private void vLCPathToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string value = @"C:\Program Files\VideoLAN\VLC\vlc.exe"; 
+            if (CHPlayer.InputBox("Save VLC Program Link", "Link to VLC Program:", ref value) == DialogResult.OK)
+            {
+                // Save Link 
+                Properties.Settings.Default.pathVLC = value;
+                // Use Link
+                sVLC = value;
+            }
+        }
+
+        //*****************************************************************************************
+        // Create Form for input 
+        //*****************************************************************************************
+        public static DialogResult InputBox(string title, string promptText, ref string value)
+        {
+            Form form = new Form();
+            Label label = new Label();
+            TextBox textBox = new TextBox();
+            Button buttonOk = new Button();
+            Button buttonCancel = new Button();
+
+            form.Text = title;
+            label.Text = promptText;
+            textBox.Text = value;
+
+            buttonOk.Text = "OK";
+            buttonCancel.Text = "Cancel";
+            buttonOk.DialogResult = DialogResult.OK;
+            buttonCancel.DialogResult = DialogResult.Cancel;
+
+            label.SetBounds(9, 20, 372, 13);
+            textBox.SetBounds(12, 36, 372, 20);
+            buttonOk.SetBounds(228, 72, 75, 23);
+            buttonCancel.SetBounds(309, 72, 75, 23);
+
+            label.AutoSize = true;
+            textBox.Anchor = textBox.Anchor | AnchorStyles.Right;
+            buttonOk.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            buttonCancel.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+
+            form.ClientSize = new Size(396, 107);
+            form.Controls.AddRange(new Control[] { label, textBox, buttonOk, buttonCancel });
+            form.ClientSize = new Size(Math.Max(300, label.Right + 10), form.ClientSize.Height);
+            form.FormBorderStyle = FormBorderStyle.FixedDialog;
+            form.StartPosition = FormStartPosition.CenterScreen;
+            form.MinimizeBox = false;
+            form.MaximizeBox = false;
+            form.AcceptButton = buttonOk;
+            form.CancelButton = buttonCancel;
+
+            DialogResult dialogResult = form.ShowDialog();
+            value = textBox.Text;
+            return dialogResult;
+        }
+
+        private void installToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //*****************************************************************************************
+        // Setup plugin for PornHub for VLC
+        //*****************************************************************************************
+        private void installToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+
+            // Setup %Appdata% Directory for VLC
+            var appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            var path = Path.Combine(appDataPath, @"vlc\lua\playlist");
+
+            // Create Directory if not Exist
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+
+            // Copy PornHub plugin File from C:\temp to %Appdata%\vlc\lua\playlist\ 
+            string sourceFile = @"C:\temp\pornhub.lua";
+            string destFile = path + @"\pornhub.lua";
+            System.IO.File.Copy(sourceFile, destFile, true);
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openVideoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            button_Sel_VideoFile_Click(sender, EventArgs.Empty);
+        }
+
+        private void openAudioToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            button_Sel_Audio_Click(sender, EventArgs.Empty);
+        }
+
+        private void setupToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //*****************************************************************************************
+        // Show About Box
+        //*****************************************************************************************
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Show AboutBox
+            AboutBox1 a = new AboutBox1();
+            a.Show();
+
+        }
+    }
+}
